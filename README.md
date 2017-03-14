54c5b77db21cd080989683ed9aa1a72fc0263d04
davidkorff <davidkorff@gmail.com>
2017-03-14 15:14:14 -0400

    added readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# AngelList cycle through Script
+
+To use, copy and paste code from index.js in the console page of AngelList after you've done a search for jobs.
+
++ To cycle through press '.' to open up company and copy cover letter.
++ Press enter to send cover letter
++ Or press ',' to skip
--- index.js
+++ index.js
@@ -1,33 +1,14 @@
 jobs = document.getElementsByClassName("djl87 job_listings fbw9 browse_startups_table_row _a _jm")
 i=0
-
-
-// document.getElementsByClassName("interested-button c-button c-button--blue c-button--loading")[1].click()
-
-// listen for tab keypress to click on item
-// listen for q keypress to click cover letter and add in writing
-// listen for a keypress to click delete
-// incriment i counter for jobs =document.getElementsByClassName("djl87 job_listings fbw9 browse_startups_table_row _a _jm")
-
 document.addEventListener("keypress", press)
-
 function press(){
   if (event.key==='.'){
-    //find open the tab
-    //jobs[i].click()
-
-  // else if (event.key==='Space'){
-    //fill out cover letter
-
     //open cover letter field
     jobs[i].children[1].children[0].children[0].children[0].children[1].children[0].children[1].children[1].children[0].click()
-
     //get name
     name = $('textarea')[i].placeholder.split(" ")[4]
-
     //get company
     company = $('textarea')[i].placeholder.split("  ")[0].split(" ").splice(6).join(" ").replace(".","")
-
     //coverletter text
     jobs[i].children[1].children[0].children[0].children[0].children[1].children[0].children[3].children[0].value = `Hi ${name},
 While earning my degree in applied physics I was employed as a project manager. It was my job to develop processes to transfer the manual process of prosthetic development into an automated one through precise scanning, modifying, and machining. After undertaking numerous challenges, and accomplishing extreme strides for the company, I decided after I earn my degree to explore new endeavors.
@@ -35,16 +16,14 @@ I’m an analyzer, a problem solver, and a thinker. I have a passion for numbers
  
 Sincerely,
 David Korff`
-
    }
   else if (event.key==='Enter'){
     //send letter
         jobs[i].children[1].children[0].children[0].children[0].children[1].children[0].children[3].children[2].click()
-
-        //incriment i
         i++
   }
   else if(event.key===','){
+    //reject company
     jobs[i].children[1].children[0].children[0].children[0].children[1].children[0].children[1].children[3].click()
     i++
   }
